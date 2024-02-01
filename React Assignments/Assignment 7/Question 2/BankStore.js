fb43a46d2648a3f8d9d0a9da4faf015009f7f75d
Harshit Dubey <hdlikes8@gmail.com>
2024-02-01 12:29:14 +0530

    Updated Assignment7

---
+++ React Assignments/Assignment 7/Question 2/BankStore.js
@@ -0,0 +1,41 @@
+import { legacy_createStore as createStore } from "redux";
+
+// Step1:   Reducer Function
+const bankReducer = (state, action) => {
+  // Convert action type to upper case
+  action.type = action.type.toUpperCase();
+
+  // alert(action.type);
+  let updatedState = {};
+
+  switch (action.type) {
+    case "CREATE":
+      updatedState.balance = 500;
+      break;
+
+    case "DEPOSIT":
+      updatedState.balance = state.balance + parseFloat(action.amount);
+      break;
+
+    case "WITHDRAW":
+      if (
+        state.balance < parseFloat(action.amount) ||
+        state.balance - parseFloat(action.amount) < 500
+      ) {
+        alert("Insufficient Fund");
+        updatedState.balance = state.balance;
+      } else {
+        updatedState.balance = state.balance - parseFloat(action.amount);
+      }
+      break;
+    default:
+      updatedState = state;
+      break;
+  }
+
+  return updatedState;
+};
+
+// Step2:  Create Store
+const bankStore = createStore(bankReducer);
+export default bankStore;
